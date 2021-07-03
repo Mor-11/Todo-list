9bcb580d99ce9ec7103efa3531e2cb687cfcc648
Zhuchyk Maryna <marinazh122000@gmail.com>
2021-07-03 23:14:34 +0300

    fead: add new element

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -4,10 +4,13 @@ import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import TodoList from "../todo-list";
 import ItemStatusFilter from "../item-status-filter";
+import ItemAddForm from "../item-add-form/item-add-form";
 
 import "./app.css";
 
 export default class App extends Component {
+  maxId = 100;
+
   state = {
     todoData: [
       { label: "Drink Coffee", important: false, id: 1 },
@@ -19,12 +22,27 @@ export default class App extends Component {
   deleteItem = (id) => {
     this.setState(({ todoData }) => {
       const idx = todoData.findIndex((el) => el.id === id);
-      const newArray = [
-        ...todoData.slice(0, idx),
-        ...todoData.slice(idx + 1),
-      ];
+
+      const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+
       return {
         todoData: newArray,
+      };
+    });
+  };
+
+  addItem = (text) => {
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++,
+    };
+
+    this.setState(({ todoData }) => {
+      const newArr = [...todoData, newItem];
+
+      return {
+        todoData: newArr,
       };
     });
   };
@@ -38,6 +56,7 @@ export default class App extends Component {
           <ItemStatusFilter />
         </div>
         <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
+        <ItemAddForm onItemAdded={this.addItem} />
       </div>
     );
   }
