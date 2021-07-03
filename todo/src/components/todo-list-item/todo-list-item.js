ab81e529757ff3fdd03fc64911a07450067d76d1
Zhuchyk Maryna <marinazh122000@gmail.com>
2021-07-03 19:14:14 +0300

    feat: add strikethrough text at click

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -3,13 +3,24 @@ import React, { Component } from "react";
 import "./todo-list-item.css";
 
 export default class TodoListItem extends Component {
-  
+  state = {
+    done: false,
+  };
+
   onLadelClick = () => {
-    console.log(`Done: ${this.props.label}`);
+    this.setState({
+      done: true,
+    });
   };
 
   render() {
     const { label, important = false } = this.props;
+    const { done } = this.state;
+
+    let className = "todo-list-item";
+    if (done) {
+      className += " done";
+    }
 
     const style = {
       color: important ? "steelblue" : "black",
@@ -17,7 +28,7 @@ export default class TodoListItem extends Component {
     };
 
     return (
-      <span className="todo-list-item">
+      <span className={className}>
         <span
           className="todo-list-item-label"
           style={style}
